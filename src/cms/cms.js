d4895d617063dab54f02c4419c33d00eb49d305d
james simone <jamirus99@gmail.com>
2020-01-03 16:27:04 -0500

    Fixed style rendering for previews in Netlify CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,45 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import CMS from "netlify-cms-app";
+import uploadcare from "netlify-cms-media-library-uploadcare";
+import cloudinary from "netlify-cms-media-library-cloudinary";
+import React, { useState, useEffect } from "react";
+import { StyleSheetManager } from "styled-components";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import ProductPagePreview from "./preview-templates/ProductPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+function StyleInjector({ children }) {
+  const [iframeRef, setIframeRef] = useState(null);
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+  useEffect(() => {
+    const iframe = document.getElementsByTagName("iframe")[0];
+    const iframeHeadElem = iframe.contentDocument.head;
+    setIframeRef(iframeHeadElem);
+  }, []);
+
+  return (
+    iframeRef && (
+      <StyleSheetManager target={iframeRef}>{children}</StyleSheetManager>
+    )
+  );
+}
+
+export default function withStyledComponents(Comp) {
+  return props => (
+    <StyleInjector>
+      <Comp {...props} />
+    </StyleInjector>
+  );
+}
+
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
+
+CMS.registerPreviewTemplate("index", withStyledComponents(IndexPagePreview));
+CMS.registerPreviewTemplate("about", withStyledComponents(AboutPagePreview));
+CMS.registerPreviewTemplate(
+  "products",
+  withStyledComponents(ProductPagePreview)
+);
+CMS.registerPreviewTemplate("blog", withStyledComponents(BlogPostPreview));
