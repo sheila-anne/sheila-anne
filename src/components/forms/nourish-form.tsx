d431caa408dd36756b135981ac0b1ba6b70c75da
james simone <jamirus99@gmail.com>
2023-06-19 19:32:04 -0400

    Updating nourish and nourish form

--- src/components/forms/nourish-form.tsx
+++ src/components/forms/nourish-form.tsx
@@ -36,7 +36,7 @@ export const NourishForm = ({
     isSubmitSuccess={isSubmitSuccess}
     formDescription="Hi there! I'm so happy you're here and I'm looking forward to learning more about you 🌼"
     formRoute={formRoute}
-    formTitle="Apply to NOURISH"
+    formTitle="Apply to Nourish"
     page={page}
     submitText={submitText}
     trackArgs={{
@@ -74,8 +74,8 @@ export const NourishForm = ({
     <TextArea
       id="how-did-you-hear-about-nourish"
       name="how-did-you-hear-about-nourish"
-      title="How did you hear about NOURISH?"
-      placeholder="How did you hear about NOURISH?"
+      title="How did you hear about Nourish?"
+      placeholder="How did you hear about Nourish?"
     />
     <TextArea
       id="how-would-you-describe-where-you-currently-are-in-your-life"
@@ -90,28 +90,28 @@ export const NourishForm = ({
       placeholder="What are your three biggest challenges when it comes to creating the life you truly desire?"
     />
     <StyledSmall>
-      Ex. Self doubt, too many options, stress / anxiety, not knowing my purpose, uncertainty about what’s next, getting
+      Ex. Self doubt, too many options, stress / anxiety, not knowing my purpose, uncertainty about what's next, getting
       in my own way, not having a supportive community
     </StyledSmall>
     <TextArea
       id="what-are-you-looking-to-get-out-of-nourish"
       name="what-are-you-looking-to-get-out-of-nourish"
-      title="What are you looking to get out of NOURISH?"
-      placeholder="What are you looking to get out of NOURISH?"
+      title="What are you looking to get out of Nourish?"
+      placeholder="What are you looking to get out of Nourish?"
     />
     <StyledSmall>
       Ex. To feel healthy & well, inner peace, a new career venture, build my own business intentionally, cultivating a
       community, feeling 100% ME
     </StyledSmall>
     <label htmlFor="are-you-ready-to-commit">
-      NOURISH is a high value program designed to transform your life and career. It’s packed with powerful content,
-      we’ll have bi-weekly immersive group coaching calls, and you have access to an intimate online community to hold
+      Nourish is a high value program designed to transform your life and career. It's packed with powerful content,
+      we'll have bi-weekly immersive group coaching calls, and you have access to an intimate online community to hold
       you every step of the way. Are you ready, willing, and able to invest in yourself financially right now, and to
       commit to nourishing the amazing woman you are?
     </label>
     <Select id="are-you-ready-to-commit" name="are-you-ready-to-commit" options={["Heck yes!", "I'm not sure ..."]} />
     <label htmlFor="will-you-schedule-the-call">
-      Once we receive your application, we'll email you to schedule a connection call with Sheila to explore if NOURISH
+      Once we receive your application, we'll email you to schedule a connection call with Sheila to explore if Nourish
       is the right program for you. Will you schedule the call and show up?
     </label>
     <Select id="will-you-schedule-the-call" name="will-you-schedule-the-call" options={["Absolutely!", "No."]} />
