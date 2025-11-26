fdaa41647bebe4a54383838291b2f7d636a842ec
James Simone <16430727+jamessimone@users.noreply.github.com>
2025-11-26 08:23:39 -0500

    Remove CMS, update dependencies (#154)

--- gatsby-config.js
+++ gatsby-config.js
@@ -163,13 +163,7 @@ module.exports = {
       options: {
         pixelId: process.env.FACEBOOK_PIXEL_ID,
       },
-    },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    }
   ],
   developMiddleware: app => {
     app.use(
